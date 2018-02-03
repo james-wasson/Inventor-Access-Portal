f29d8117c3d9728ec082e3e9668686074628eff1
James Adam Wasson <jameswasson1@gmail.com>
2018-02-03 13:25:29 -0600

    remove unnecessary models and adding frist DB support

--- InventorAccessPortal.Web/App_Start/Startup.Auth.cs
+++ InventorAccessPortal.Web/App_Start/Startup.Auth.cs
@@ -5,7 +5,6 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
-using InventorAccessPortal.Web.Models;
 
 namespace InventorAccessPortal.Web
 {
@@ -14,11 +13,6 @@ namespace InventorAccessPortal.Web
         // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
-
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             // Configure the sign in cookie
@@ -28,11 +22,7 @@ namespace InventorAccessPortal.Web
                 LoginPath = new PathString("/Account/Login"),
                 Provider = new CookieAuthenticationProvider
                 {
-                    // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+
                 }
             });            
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
---
+++ InventorAccessPortal.Web/DB/Controllers/Connections.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Web;
+using System.Web.UI;
+using System.Configuration;
+
+namespace InventorAccessPortal.Web.DB.Controllers
+{
+    public class Connections
+    {
+        /// <summary>
+        /// A touple storing the connection string, Ole connection, and name
+        /// </summary>
+        public class DBConnection {
+            public string ConnectionString
+            {
+                get;
+            }
+            public string Name
+            {
+                get;
+            }
+            public string ProviderName
+            {
+                get;
+            }
+            public OleDbConnection Connection
+            {
+                get;
+            }
+            // public constructor
+            public DBConnection(OleDbConnection conn, string connString, string name, string providerName = "") {
+                ConnectionString = connString;
+                Name = name;
+                ProviderName = providerName;
+                Connection = conn;
+            }
+        }
+
+        private List<DBConnection> DBConnectionsList = new List<DBConnection>();
+
+        /// <summary>
+        /// Defult connection of database
+        /// </summary>
+        public Connections()
+        {
+            foreach (ConnectionStringSettings css in ConfigurationManager.ConnectionStrings)
+            {
+                if (css.ConnectionString != null && css.ConnectionString != "")
+                {
+                    try
+                    {
+                        // copies the connectionstring so we can modify it
+                        string connString = css.ConnectionString;
+                        // if not provider is specifed in the string
+                        if (!connString.ToLower().Contains("provider="))
+                        {
+                            // use the one in the config file if it exists
+                            if (css.ProviderName != null && css.ProviderName != "")
+                            {
+                                connString = css.ConnectionString + ";Provider=" + css.ProviderName;
+                            }
+                            else // if no provider name can be found throw an error
+                            {
+                                throw new Exception("No Provider Name is in either connection string or in settings");
+                            }
+                        }
+                        // try the connection
+                        var connection = new DBConnection(
+                            new OleDbConnection(connString),
+                            connString,
+                            css.Name,
+                            css.ProviderName
+                        );
+                        // adds it to the connection string list
+                        DBConnectionsList.Add(connection);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("DataBase Connection failed" +
+                            Environment.NewLine +
+                            "Connection String: " + (css.ConnectionString != null ? css.ConnectionString : "Error: Can't Read") +
+                            Environment.NewLine +
+                            "Name: " + (css.Name != null ? css.Name : "Error: Can't Read") +
+                            Environment.NewLine +
+                            "Provider Name: " + (css.ProviderName != null ? css.ProviderName : "Error: Can't Read") +
+                            Environment.NewLine +
+                            "Exception: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        public List<DBConnection> GetConnections() {
+            return DBConnectionsList;
+        }
+
+        /**************************************
+
+        /// <summary>
+        /// execute SQL command which don't have a feedback result
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <returns>the sgin of whether succeed</returns>
+        public bool ExeSQL(string strSQL)
+        {
+            bool resultState = false;
+
+            Connection.Open();
+            OleDbTransaction myTrans = Connection.BeginTransaction();
+            OleDbCommand command = new OleDbCommand(strSQL, Connection, myTrans);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                myTrans.Commit();
+                resultState = true;
+            }
+            catch
+            {
+                myTrans.Rollback();
+                resultState = false;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return resultState;
+        }
+
+        /// <summary>
+        /// execute SQL command and send result back to DataReader
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <returns>dataReader</returns>
+        private OleDbDataReader ReturnDataReader(string strSQL)
+        {
+            Connection.Open();
+            OleDbCommand command = new OleDbCommand(strSQL, Connection);
+            OleDbDataReader dataReader = command.ExecuteReader();
+            Connection.Close();
+
+            return dataReader;
+        }
+
+        /// <summary>
+        /// execute SQL command and send result back to DataSet
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <returns>DataSet</returns>
+        public DataSet ReturnDataSet(string strSQL)
+        {
+            Connection.Open();
+            DataSet dataSet = new DataSet();
+            OleDbDataAdapter OleDbDA = new OleDbDataAdapter(strSQL, Connection);
+            OleDbDA.Fill(dataSet, "objDataSet");
+
+            Connection.Close();
+            return dataSet;
+        }
+
+        /// <summary>
+        /// Execute a search SQL command and return the number of results
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <returns>sqlResultCount</returns>
+        public int ReturnSqlResultCount(string strSQL)
+        {
+            int sqlResultCount = 0;
+
+            try
+            {
+                Connection.Open();
+                OleDbCommand command = new OleDbCommand(strSQL, Connection);
+                OleDbDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    sqlResultCount++;
+                }
+                dataReader.Close();
+            }
+            catch
+            {
+                sqlResultCount = 1;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+            return sqlResultCount;
+        }
+
+        *****************************************************/
+    }
+}
