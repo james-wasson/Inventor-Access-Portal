b64958de3f93e2342874cdb24a5aa6d0e30498eb
binlinquge <714384807@qq.com>
2018-02-03 18:20:16 -0600

    Convert the connection functions. Create a Test Controller, a Test View, a Test Model to test the connection to the DB and functions in Connection.cs

---
+++ InventorAccessPortal.Web/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventorAccessPortal.Web.DB.Objects;
+using InventorAccessPortal.Web.Models;
+
+namespace InventorAccessPortal.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var test = new Test() { Mydb = new Connection(System.Configuration.ConfigurationManager.ConnectionStrings["Access.DB.1"].ToString(), "Access.DB.1", "Microsoft.ACE.OLEDB.12.0") };
+            return View(test);
+        }
+    }
+}
--- InventorAccessPortal.Web/DB/Objects/Connection.cs
+++ InventorAccessPortal.Web/DB/Objects/Connection.cs
@@ -73,6 +73,101 @@ namespace InventorAccessPortal.Web.DB.Objects
             }
         }
 
+        /// <summary>
+        /// execute SQL command which don't have a feedback result
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <returns>the sgin of whether succeed</returns>
+        public bool ExeSQL(string strSQL)
+        {
+            bool resultState = false;
+
+            DBConnection.Open();
+            OleDbTransaction myTrans = DBConnection.BeginTransaction();
+            OleDbCommand command = new OleDbCommand(strSQL, DBConnection, myTrans);
+
+            try
+            {
+                command.ExecuteNonQuery();
+                myTrans.Commit();
+                resultState = true;
+            }
+            catch
+            {
+                myTrans.Rollback();
+                resultState = false;
+            }
+            finally
+            {
+                DBConnection.Close();
+            }
+            return resultState;
+        }
+        /// <summary>
+        /// execute SQL command and send result back to DataReader
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <returns>dataReader</returns>
+        private OleDbDataReader ReturnDataReader(string strSQL)
+        {
+            DBConnection.Open();
+            OleDbCommand command = new OleDbCommand(strSQL, DBConnection);
+            OleDbDataReader dataReader = command.ExecuteReader();
+            DBConnection.Close();
+
+            return dataReader;
+        }
+
+        /// <summary>
+        /// execute SQL command and send result back to DataSet
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <returns>DataSet</returns>
+        public DataSet ReturnDataSet(string strSQL)
+        {
+            DBConnection.Open();
+            DataSet dataSet = new DataSet();
+            OleDbDataAdapter OleDbDA = new OleDbDataAdapter(strSQL, DBConnection);
+            OleDbDA.Fill(dataSet, "objDataSet");
+
+            DBConnection.Close();
+            return dataSet;
+        }
+
+        /// <summary>
+        /// Execute a search SQL command and return the number of results
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <returns>sqlResultCount</returns>
+        public int ReturnSqlResultCount(string strSQL)
+        {
+            int sqlResultCount = 0;
+
+            try
+            {
+                DBConnection.Open();
+                OleDbCommand command = new OleDbCommand(strSQL, DBConnection);
+                OleDbDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    sqlResultCount++;
+                }
+                dataReader.Close();
+            }
+            catch
+            {
+                sqlResultCount = 1;
+            }
+            finally
+            {
+                DBConnection.Close();
+            }
+            return sqlResultCount;
+        }
+
+
+
         public void Dispose()
         {
             DBConnection.Dispose();
