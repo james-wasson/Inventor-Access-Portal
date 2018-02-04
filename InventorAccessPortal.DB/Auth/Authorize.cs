770eb4f10e8bb54724da1e820b5cbd4edf145259
James Adam Wasson <jameswasson1@gmail.com>
2018-02-04 04:26:24 -0600

    Adding new DB access methods
    Removing old DB looping methods

--- InventorAccessPortal.DB/Auth/Authorize.cs
+++ InventorAccessPortal.DB/Auth/Authorize.cs
@@ -4,16 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using InventorAccessPortal.DB._InventorAccessPortal_1DataSetTableAdapters;
+using InventorAccessPortal.DB._DB1_DataSetTableAdapters;
 
 namespace InventorAccessPortal.DB.Auth
 {
     public class Authorize : Context
     {
         public bool ByUsername(String username) {
-            foreach (var conn in GetConnections())
+            All_InvestigatorsTableAdapter users = new All_InvestigatorsTableAdapter();
+            var data = users.GetData();
+            foreach (var row in data.Rows)
             {
-                //All_InvestigatorsTableAdapter users = new All_InvestigatorsTableAdapter();
+                var a = row;
             }
             return false;
         }
