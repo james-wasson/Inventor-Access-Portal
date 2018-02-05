dfeba658c14b2feca47340b9c2efeb17904cd667
James Adam Wasson <jameswasson1@gmail.com>
2018-02-05 14:27:21 -0600

    switching bundle dirctories

--- InventorAccessPortal.Web/App_Start/BundleConfig.cs
+++ InventorAccessPortal.Web/App_Start/BundleConfig.cs
@@ -8,24 +8,35 @@ namespace InventorAccessPortal.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/Content/css")
+                .IncludeDirectory("~/Content/", "*.css", false));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                .IncludeDirectory("~/Scripts/", "*.js", false));
+
+            /*
+            * Libaray Bundles 
+            */
+
+            bundles.Add(new ScriptBundle("~/bundles/Lib/jquery").Include(
+                        "~/Scripts/Lib/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Lib/jqueryval").Include(
+                        "~/Scripts/Lib/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/Lib/modernizr").Include(
+                        "~/Scripts/Lib/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/Lib/bootstrap").Include(
+                      "~/Scripts/Lib/bootstrap.js",
+                      "~/Scripts/Lib/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/Lib/css").Include(
+                      "~/Content/Lib/bootstrap.css",
+                      "~/Content/Lib/site.css"));
         }
     }
 }
