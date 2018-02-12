c8975cfc7318447210ecee7f26e3dae681afa303
N3rdizm <33732911+N3rdizm@users.noreply.github.com>
2018-02-12 15:49:14 -0600

    Navbar css and new views
    preliminary views with some navbar css in the works

--- InventorAccessPortal.Web/Controllers/HomeController.cs
+++ InventorAccessPortal.Web/Controllers/HomeController.cs
@@ -27,5 +27,33 @@ namespace InventorAccessPortal.Web.Controllers
 
             return View();
         }
+
+        public ActionResult RecentActivities()
+        {
+            ViewBag.Message = "Recent activities relevant to you.";
+
+            return View();
+        }
+
+        public ActionResult InventionsForm()
+        {
+            ViewBag.Message = "All Inventions";
+
+            return View();
+        }
+
+        public ActionResult FilesForm()
+        {
+            ViewBag.Message = "All Files";
+
+            return View();
+        }
+
+        public ActionResult FamiliesForm()
+        {
+            ViewBag.Message = "All Families";
+
+            return View();
+        }
     }
 }
