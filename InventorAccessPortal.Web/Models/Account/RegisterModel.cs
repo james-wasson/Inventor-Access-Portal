b2a30d560e0ec740e1bed1cabba2cb62881babac
N3rdizm <33732911+N3rdizm@users.noreply.github.com>
2018-03-05 15:53:43 -0600

    Adding Register page format stuff

---
+++ InventorAccessPortal.Web/Models/Account/RegisterModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using InventorAccessPortal.Web.Util;
+
+namespace InventorAccessPortal.Web.Models.Account
+{
+    public enum RegisterErrorCodes
+    {
+        [Description("Invalid password.")]
+        InvalidPassword = 0,
+        [Description("Username or password fields are empty.")]
+        UsernameOrPasswordEmpty = 1
+    }
+    public class RegisterModel : ErrorModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordVerify { get; set; }
+    }
+}
