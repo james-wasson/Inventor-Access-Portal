3b8391cd417ff0572ba99d15e49075ba51c57c1e
N3rdizm <33732911+N3rdizm@users.noreply.github.com>
2018-03-07 15:16:58 -0600

    Changing Register model stuff

---
+++ InventorAccessPortal.Web/Models/Account/RegisterModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using InventorAccessPortal.Web.Util;
+
+namespace InventorAccessPortal.Web.Models.Account
+{
+    public enum RegisterErrorCodes
+    {
+        [Description("Invalid Username or Password.")]
+        InvalidUsernameOrPassword = 0,
+        [Description("Username or Password is empty.")]
+        EmptyUsernameOrPassword = 1
+    }
+    public class RegisterModel : SharedModel
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordVerify { get; set; }
+    }
+} 
