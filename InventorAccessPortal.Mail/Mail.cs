4a52a95cbddfe2ea2e841dfa1663530e1d450f7b
james-wasson <jameswasson1@gmail.com>
2018-03-16 16:23:57 -0500

    adding basic structure of the mailer

---
+++ InventorAccessPortal.Mail/Mail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net;
+using System.Net.Configuration;
+using System.Net.Mail;
+
+namespace InventorAccessPortal.Mail
+{
+    public class SmtpConfig {
+        public void send(SmtpClient client)
+        {
+            
+        }
+    }
+}
