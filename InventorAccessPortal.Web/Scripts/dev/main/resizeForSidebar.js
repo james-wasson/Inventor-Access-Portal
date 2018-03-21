ba4d248ca51ca6867326c636d62d874a3d7d5746
james-wasson <jameswasson1@gmail.com>
2018-03-21 13:44:57 -0500

    adding dynamic footer

---
+++ InventorAccessPortal.Web/Scripts/dev/main/resizeForSidebar.js
@@ -0,0 +1,8 @@
+﻿$().ready(function () {
+    $sidebar = $("#sidebar");
+    $space = $("#spaceForSidebar");
+    function resizeContent() {
+        $space.css("min-width", $sidebar.outerWidth() + "px");
+    } resizeContent();
+    addResizeListener($sidebar[0], resizeContent);
+});
