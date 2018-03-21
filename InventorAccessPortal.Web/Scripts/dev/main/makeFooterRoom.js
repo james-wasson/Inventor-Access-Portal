6daa58062d1c0a19b57c5eae8bc6489af83daa4c
james-wasson <jameswasson1@gmail.com>
2018-03-21 12:21:53 -0500

    making sidebar fixed

--- InventorAccessPortal.Web/Scripts/dev/main/makeFooterRoom.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $content = $("#content");
-    $footer = $("#footer");
-    function resizeContent() {
-        $content.css('max-height', ($(window).height() - $footer.height()) + "px");
-    } resizeContent();
-    addResizeListener($("#content")[0], resizeContent);
-    $(window).resize(resizeContent);
-});
