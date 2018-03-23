5672b7b1c8d540e18e713b34e06a85e4de6fc027
james-wasson <jameswasson1@gmail.com>
2018-03-23 12:18:46 -0500

    adding some minor styling

--- InventorAccessPortal.Web/Scripts/dev/main/resizeForContent.js
+++ InventorAccessPortal.Web/Scripts/dev/main/resizeForContent.js
@@ -1,9 +1,13 @@
 ﻿$().ready(function () {
     var $footer = $("#footer");
     var $space = $("#spaceForFooter");
-    var $content = $("#content-body");
+    var $body = $("#content-body");
+    var $content = $("#content");
     function resizeContent() {
+        var spaceleft = $content.innerHeight();
         $space.css("min-height", $footer.outerHeight() + "px");
+        spaceleft -= $space.outerHeight();
+        $body.css("min-height", spaceleft + "px");
     } resizeContent();
     addResizeListener($footer[0], resizeContent);
 });
